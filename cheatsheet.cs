0792813f0c8324a299850fb58dbc55b59710d590
Robert Steven Tomas <steven.tomas@live.com>
2016-03-13 18:56:28 +0800

    no message

--- cheatsheet.cs
+++ cheatsheet.cs
@@ -181,3 +181,114 @@ class='<%# Eval("AmbulanceStatus").ToString() == "Active" ? "btn btn-success btn
 Helper.LogException(Session["userid"].ToString(), "Membership Renewal, Automatic Renewal Mail ",
                             "Exception Type: " + ex.GetType().ToString() + " " +
                             "Exception Message: " + ex.Message.ToString());
+
+//Reports
+//ddl
+<div class="row">
+    <div class="col-lg-8">
+        <div class="col-lg-5">
+            <div class="form-group">
+                <label class="control-label col-lg-4">Initial Date</label>
+                <div class="col-lg-8">
+                    <asp:TextBox ID="txtDate1" class="form-control" runat="server" TextMode="Date" required></asp:TextBox>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5">
+            <div class="form-group">
+                <label class="control-label col-lg-4">End Date</label>
+                <div class="col-lg-8">
+                    <asp:TextBox ID="txtDate2" class="form-control" runat="server" TextMode="Date" required></asp:TextBox>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2">
+            <div class="form-group">
+                <div class="col-lg-10">
+                    <asp:Button ID="btnSubmit" runat="server" Text="Submit" CssClass="btn btn-default" OnClick="btnSubmit_Click" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+//CS
+//declare in class
+private string name;
+private string type;
+
+//Page load
+Helper.ValidateAdmin();
+if (!IsPostBack)
+{
+    MinMaxDate();
+}
+GetReports();
+
+//MinMax
+void MinMaxDate()
+{
+    using (SqlConnection con = new SqlConnection(Helper.GetCon()))
+    using (SqlCommand cmd = new SqlCommand())
+    {
+        con.Open();
+        cmd.Connection = con;
+        cmd.CommandText = "SELECT MIN(DateAdded) AS Min, MAX(DateAdded) As Max  " +
+        "FROM Users";
+        using (SqlDataReader dr = cmd.ExecuteReader())
+        {
+            dr.Read();
+            if (dr.HasRows)
+            {
+                DateTime DateOne = Convert.ToDateTime(dr["Min"].ToString());
+                txtDate1.Text = DateOne.ToString("yyyy-MM-dd");
+                DateTime DateTwo = Convert.ToDateTime(dr["Max"].ToString());
+                txtDate2.Text = DateTwo.ToString("yyyy-MM-dd");
+            }
+        }
+    }
+}
+
+//Get method
+void GetReports()
+{
+    using (SqlConnection con = new SqlConnection(Helper.GetCon()))
+    using (SqlCommand cmd = new SqlCommand())
+    {
+        con.Open();
+        cmd.Connection = con;
+
+        cmd.CommandText = "SELECT LastName + ', ' + FirstName AS Name, UserType FROM Users " +
+        "INNER JOIN Types ON Users.TypeID=Types.TypeID WHERE UserID=@UserID";
+        cmd.Parameters.AddWithValue("@UserID", Session["userid"].ToString());
+        using (SqlDataReader dr = cmd.ExecuteReader())
+        {
+            while (dr.Read())
+            {
+                name = dr["Name"].ToString();
+                type = dr["UserType"].ToString();
+            }
+        }
+
+        ReportDocument report = new ReportDocument(); 
+        report.Load(Server.MapPath("~/Admin/Users/Reports/rptReports.rpt"));
+
+        if (Helper.Secured() != "true")
+        report.DataSourceConnections[0].SetConnection(Helper.GetCrServer(), Helper.GetCrDb(), true);
+        else
+        report.DataSourceConnections[0].SetConnection(Helper.GetCrServer(), Helper.GetCrDb(), Helper.GetCrvUser(), Helper.GetCrvPass());
+
+        report.SetParameterValue("Name", name);
+        report.SetParameterValue("UserType", type);
+        report.SetParameterValue("Date1", txtDate1.Text);
+        report.SetParameterValue("Date2", txtDate2.Text);
+
+        crvReports.ReportSource = report;
+        crvReports.DataBind();
+    }
+}
+
+protected void btnSubmit_Click(object sender, EventArgs e)
+{
+    GetReports();
+}
+
